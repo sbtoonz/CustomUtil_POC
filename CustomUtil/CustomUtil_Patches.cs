a2ef1072460549d95de53abe6e16e1e95eefedc3
sbtoonz <67915879+sbtoonz@users.noreply.github.com>
2023-05-22 17:51:31 -0700

    Transpiler inserts method and args passed work as expected

---
+++ CustomUtil/CustomUtil_Patches.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+
+namespace CustomUtil
+{
+    public class CustomUtil_Patches
+    {
+        [HarmonyPatch(typeof(ItemDrop), nameof(ItemDrop.Awake))]
+        public static class AssignLanterTypePatch
+        {
+            public static void Postfix(ItemDrop __instance)
+            {
+                if (__instance.gameObject.name == "Lantern")
+                {
+                    if(__instance.m_itemData.m_customData.ContainsKey("HipLantern"))return;
+                    __instance.m_itemData.m_customData.Add("HipLantern", "1");
+                }
+            }
+        }
+    }
+}
--- CustomUtil/CustomUtil_Transpilers.cs
+++ CustomUtil/CustomUtil_Transpilers.cs
@@ -1,7 +1,53 @@
-﻿namespace CustomUtil
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using HarmonyLib;
+using UnityEngine;
+
+namespace CustomUtil
 {
+    
+    /*
+     IL_00fe: ldarg.0      // this
+    IL_00ff: ldnull
+    IL_0100: stfld        class ItemDrop/ItemData Humanoid::m_hiddenLeftItem
+
+    IL_0105: br           IL_047b
+
+    // [568 10 - 568 75]
+    IL_010a: ldarg.1      // item
+     */
     public class CustomUtil_Transpilers
     {
-        
+        [HarmonyPatch(typeof(Humanoid), nameof(Humanoid.EquipItem))]
+        public static class EquipItemTranspiler
+        {
+            [HarmonyTranspiler]
+            public static IEnumerable<CodeInstruction> EquipItemsTranspiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var cm = new CodeMatcher(instructions)
+                    .MatchForward(useEnd: false,
+                        new CodeMatch(OpCodes.Ldarg_0),
+                        new CodeMatch(OpCodes.Ldnull),
+                        new CodeMatch(OpCodes.Stfld, AccessTools.Field(typeof(Humanoid), nameof(Humanoid.m_hiddenLeftItem))),
+                        new CodeMatch(OpCodes.Br)
+                        )
+                    .Advance(9)
+                    .InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_0))
+                    .InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_1))
+                    .InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_2))
+                    .InsertAndAdvance(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(EquipItemTranspiler), nameof(EquipItemTranspiler.TestMethod))))
+                    .InstructionEnumeration();
+                return cm;
+            }
+
+            public static void TestMethod(Humanoid humanoid, ItemDrop.ItemData data, bool useEffects)
+            {
+                if (data.m_dropPrefab.name.StartsWith("Lantern"))
+                {
+                    Debug.LogWarning("Found the lantern you should do shit with it");
+                }
+            }
+        }
     }
 }
